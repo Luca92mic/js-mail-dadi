f53a49d521e2ee62e21c1f4e2ef954ff50ef2b30
Luca <luca.mic92@gmail.com>
2021-05-28 16:23:23 +0200

    definizione vincitore

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 //numero casuale utente
-document.getElementById("utente").innerHTML = Math.floor(Math.random() * 6) + 1;
+var utente=Math.round(Math.random() * 5 + 1);
+
+document.getElementById("utente").innerHTML = "Il dado Utente è: " + utente + ";";
 
 //numero casuale pc
-document.getElementById("pc").innerHTML = Math.floor(Math.random() * 6) + 1;
+var pc=Math.round(Math.random() * 5 + 1);
+
+document.getElementById("pc").innerHTML = "Il dado PC è: " + pc + ";";
+
+//definizione vincitore
+if (utente == pc) {
+
+    document.getElementById('vincitore').innerHTML = '' + 'Pareggio';
+
+} else if (utente > pc) {
+
+    document.getElementById('vincitore').innerHTML = '' + 'Il vincitore è Utente';
+} else {
+
+    document.getElementById('vincitore').innerHTML = '' + 'Il vincitore è PC';
+}
