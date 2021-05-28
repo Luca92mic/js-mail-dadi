20a3b55a733751e011ccad004ce90996ccf7ed4a
Luca <luca.mic92@gmail.com>
2021-05-28 15:58:25 +0200

    init + num random utente e pc

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//numero casuale utente
+document.getElementById("utente").innerHTML = Math.floor(Math.random() * 6) + 1;
+
+//numero casuale pc
+document.getElementById("pc").innerHTML = Math.floor(Math.random() * 6) + 1;
